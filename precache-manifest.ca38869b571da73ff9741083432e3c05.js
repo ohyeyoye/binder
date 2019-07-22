099b3785c618ba0d0d28fd3b90492852c9020623
ohyeyoye <dev.sangjun@gmail.com>
2019-07-22 11:08:04 +0900

    Updates

---
+++ precache-manifest.ca38869b571da73ff9741083432e3c05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f738c295cf7e19364b48f1fcd516a660",
+    "url": "/binder/index.html"
+  },
+  {
+    "revision": "bcc15ef2e53ebad34a07",
+    "url": "/binder/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1d42e4b01eb5bda1739a",
+    "url": "/binder/static/js/2.59de7db3.chunk.js"
+  },
+  {
+    "revision": "bcc15ef2e53ebad34a07",
+    "url": "/binder/static/js/main.f20a49f6.chunk.js"
+  },
+  {
+    "revision": "1c8e956435e6eb52eadf",
+    "url": "/binder/static/js/runtime~main.ffd2b221.js"
+  }
+]);
